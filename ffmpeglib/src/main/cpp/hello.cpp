cc2e3670a74b0be0e0ca76922d7ff79a35a051ca
liwenfeng <liwenfeng@ava.com.cn>
2019-07-11 19:56:08 +0800

    增加ffmpeg

--- ffmpeglib/src/main/cpp/hello.cpp
+++ ffmpeglib/src/main/cpp/hello.cpp
@@ -1,7 +1,11 @@
 #include <jni.h>
 #include <string>
 
-
+extern "C" {
+#include <libavformat/avformat.h>
+#include <libavcodec/avcodec.h>
+#include <libswscale/swscale.h>
+}
 extern "C" JNIEXPORT jstring
 
 JNICALL
@@ -9,5 +13,6 @@ Java_com_wenfengtou_ffmpeglib_HelloJniHelper_hello(
         JNIEnv *env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
+    av_register_all();
     return env->NewStringUTF(hello.c_str());
 }
