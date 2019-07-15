1f8478fb7dcde702b1d9119d1099dbc6a068e09f
liwenfeng <liwenfeng@ava.com.cn>
2019-07-15 16:06:29 +0800

    增加播放mp4文件，需要增加延时，不然会报尺寸错误

--- app/src/main/java/com/wenfengtou/fengtouplayer/FFmpegDrawActivity.java
+++ app/src/main/java/com/wenfengtou/fengtouplayer/FFmpegDrawActivity.java
@@ -2,6 +2,8 @@ package com.wenfengtou.fengtouplayer;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+
 import com.wenfengtou.fengtouplayer.view.MyVideoView;
 
 public class FFmpegDrawActivity extends Activity {
@@ -11,6 +13,19 @@ public class FFmpegDrawActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ffmpeg_draw);
         myVideoView = (MyVideoView) findViewById(R.id.myVV);
-        myVideoView.startPlay("http://ivi.bupt.edu.cn/hls/cctv1hd.m3u8");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //http://ivi.bupt.edu.cn/hls/cctv1hd.m3u8
+                myVideoView.startPlay("/sdcard/test.mp4");
+            }
+        },1000);
+
+
     }
 }
